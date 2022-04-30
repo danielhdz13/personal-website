03adac7c8c5e7454a0d95fef717616242bbbc439
danielhdz13 <55956673+danielhdz13@users.noreply.github.com>
2022-04-30 17:33:27 -0500

    Finished Part 4 of the Tutorial

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,17 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+      title: "Daniel Hernandez",
+      siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
-}
+  plugins: [
+      "gatsby-plugin-image",
+      "gatsby-plugin-sharp",
+      {
+          resolve: "gatsby-source-filesystem",
+          options: {
+              name: `blog`,
+              path: `${__dirname}/blog`,
+          }
+      },
+  ],
+};
---
+++ src/pages/about.js
@@ -0,0 +1,18 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+    return (
+        <Layout pageTitle="About Me">
+            <p> Hi there, my name is Daniel. I am a senior studying Computer Science
+                at Northwestern University. I am currently taking coursework in Distributed
+                Systems, Kernel Development, and Online Marketplaces. In August, I will
+                be joining Netflix as a Software Engineer in the Encoding Technologies, Media
+                Systems team. 
+            </p>
+        </Layout>
+    )
+}
+
+export default AboutPage
